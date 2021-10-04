7640da6b0a3783fd08e9d2621a5f5a41c62cfd05
Sangeet Shrestha <82662886+sangeeetttt@users.noreply.github.com>
2021-10-04 12:49:00 +0545

    Added functions

--- AccountMS.py
+++ AccountMS.py
@@ -4,7 +4,7 @@ import tkinter.ttk as ttk
 import tkinter.messagebox as tkMessageBox
 
 root = Tk()
-root.title("Python: Simple CRUD Applition")
+root.title("Account Management System by SangeetShrestha")
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 width = 900
@@ -15,9 +15,9 @@ root.geometry('%dx%d+%d+%d' % (width, height, x, y))
 root.resizable(0, 0)
 
 #==================================METHODS============================================
-def Database():
+def Database() :
     global conn, cursor
-    conn = sqlite3.connect('pythontut.db')
+    conn = sqlite3.connect('account.db')
     cursor = conn.cursor()
     cursor.execute("CREATE TABLE IF NOT EXISTS `member` (mem_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, firstname TEXT, lastname TEXT, gender TEXT, address TEXT, username TEXT, password TEXT)")
     
@@ -27,11 +27,6 @@ def Create():
     else:
         Database()
         cursor.execute("INSERT INTO `member` (firstname, lastname, gender, address, username, password) VALUES(?, ?, ?, ?, ?, ?)", (str(FIRSTNAME.get()), str(LASTNAME.get()), str(GENDER.get()), str(ADDRESS.get()), str(USERNAME.get()), str(PASSWORD.get())))
-        tree.delete(*tree.get_children())
-        cursor.execute("SELECT * FROM `member` ORDER BY `lastname` ASC")
-        fetch = cursor.fetchall()
-        for data in fetch:
-            tree.insert('', 'end', values=(data[0], data[1], data[2], data[3], data[4], data[5], data[6]))
         conn.commit()
         FIRSTNAME.set("")
         LASTNAME.set("")
@@ -49,10 +44,17 @@ def Read():
     cursor.execute("SELECT * FROM `member` ORDER BY `lastname` ASC")
     fetch = cursor.fetchall()
     for data in fetch:
-        tree.insert('', 'end', values=(data[0], data[1], data[2], data[3], data[4], data[5], data[6]))
+        tree.insert('', 'end', values=(data[1], data[2], data[3], data[4], data[5], data[6]))
     cursor.close()
     conn.close()
     txt_result.config(text="Successfully read the data from database", fg="black")
+    
+def Exit():
+    result = tkMessageBox.askquestion('Account Management System', 'Are you sure you want to exit?', icon="warning")
+    if result == 'yes':
+        root.destroy()
+        exit()
+
 
 def Update():
     Database()
@@ -60,7 +62,10 @@ def Update():
         txt_result.config(text="Please select a gender", fg="red")
     else:
         tree.delete(*tree.get_children())
-        cursor.execute("UPDATE `member` SET `firstname` = ?, `lastname` = ?, `gender` =?,  `address` = ?,  `username` = ?, `password` = ? WHERE `mem_id` = ?", (str(FIRSTNAME.get()), str(LASTNAME.get()), str(GENDER.get()), str(ADDRESS.get()), str(USERNAME.get()), str(PASSWORD.get()), int(mem_id)))
+        cursor.execute(
+            "UPDATE `member` SET `firstname` = ?, `lastname` = ?, `gender` =?,  `address` = ?,  `username` = ?, `password` = ? WHERE `mem_id` = ?",
+            (str(FIRSTNAME.get()), str(LASTNAME.get()), str(GENDER.get()), str(ADDRESS.get()), str(USERNAME.get()),
+             str(PASSWORD.get()), int(mem_id)))
         conn.commit()
         cursor.execute("SELECT * FROM `member` ORDER BY `lastname` ASC")
         fetch = cursor.fetchall()
@@ -84,7 +89,7 @@ def Update():
 def OnSelected(event):
     global mem_id;
     curItem = tree.focus()
-    contents =(tree.item(curItem))
+    contents = (tree.item(curItem))
     selecteditem = contents['values']
     mem_id = selecteditem[0]
     FIRSTNAME.set("")
@@ -103,14 +108,16 @@ def OnSelected(event):
     btn_update.config(state=NORMAL)
     btn_delete.config(state=DISABLED)
 
+
 def Delete():
     if not tree.selection():
-       txt_result.config(text="Please select an item first", fg="red")
+        txt_result.config(text="Please select an item first", fg="red")
     else:
-        result = tkMessageBox.askquestion('Python: Simple CRUD Applition', 'Are you sure you want to delete this record?', icon="warning")
+        result = tkMessageBox.askquestion('Account Management by Sangeet',
+                                          'Are you sure you want to delete this record?', icon="warning")
         if result == 'yes':
             curItem = tree.focus()
-            contents =(tree.item(curItem))
+            contents = (tree.item(curItem))
             selecteditem = contents['values']
             tree.delete(curItem)
             Database()
@@ -119,13 +126,7 @@ def Delete():
             cursor.close()
             conn.close()
             txt_result.config(text="Successfully deleted the data", fg="black")
-            
-    
-def Exit():
-    result = tkMessageBox.askquestion('Python: Simple CRUD Applition', 'Are you sure you want to exit?', icon="warning")
-    if result == 'yes':
-        root.destroy()
-        exit()
+
 
 #==================================VARIABLES==========================================
 FIRSTNAME = StringVar()
@@ -136,35 +137,35 @@ USERNAME = StringVar()
 PASSWORD = StringVar()
 
 #==================================FRAME==============================================
-Top = Frame(root, width=900, height=50, bd=8, relief="raise")
+Top = Frame(root, width=900, height=50, bd=2, relief="raise")
 Top.pack(side=TOP)
-Left = Frame(root, width=300, height=500, bd=8, relief="raise")
+Left = Frame(root, width=300, height=500, bd=2, relief="raise")
 Left.pack(side=LEFT)
-Right = Frame(root, width=600, height=500, bd=8, relief="raise")
+Right = Frame(root, width=600, height=500, bd=2, relief="raise")
 Right.pack(side=RIGHT)
 Forms = Frame(Left, width=300, height=450)
 Forms.pack(side=TOP)
-Buttons = Frame(Left, width=300, height=100, bd=8, relief="raise")
+Buttons = Frame(Left, width=300, height=100, bd=2, relief="raise")
 Buttons.pack(side=BOTTOM)
 RadioGroup = Frame(Forms)
-Male = Radiobutton(RadioGroup, text="Male", variable=GENDER, value="Male", font=('arial', 16)).pack(side=LEFT)
-Female = Radiobutton(RadioGroup, text="Female", variable=GENDER, value="Female", font=('arial', 16)).pack(side=LEFT)
+Male = Radiobutton(RadioGroup, text="Male", variable=GENDER, value="Male", font=('Calibri', 16)).pack(side=LEFT)
+Female = Radiobutton(RadioGroup, text="Female", variable=GENDER, value="Female", font=('Calibri', 16)).pack(side=LEFT)
 
 #==================================LABEL WIDGET=======================================
-txt_title = Label(Top, width=900, font=('arial', 24), text = "Python: Simple CRUD Application")
+txt_title = Label(Top, width=900, font=('Calibri', 24,'bold'), text = "Account Management System", bg='#ff5733', fg='#a9fc30')
 txt_title.pack()
-txt_firstname = Label(Forms, text="Firstname:", font=('arial', 16), bd=15)
-txt_firstname.grid(row=0, sticky="e")
-txt_lastname = Label(Forms, text="Lastname:", font=('arial', 16), bd=15)
-txt_lastname.grid(row=1, sticky="e")
-txt_gender = Label(Forms, text="Gender:", font=('arial', 16), bd=15)
-txt_gender.grid(row=2, sticky="e")
-txt_address = Label(Forms, text="Address:", font=('arial', 16), bd=15)
-txt_address.grid(row=3, sticky="e")
-txt_username = Label(Forms, text="Username:", font=('arial', 16), bd=15)
-txt_username.grid(row=4, sticky="e")
-txt_password = Label(Forms, text="Password:", font=('arial', 16), bd=15)
-txt_password.grid(row=5, sticky="e")
+txt_firstname = Label(Forms, text="Firstname:", font=('Calibri', 16), bd=16)
+txt_firstname.grid(row=0, stick="e")
+txt_lastname = Label(Forms, text="Lastname:", font=('Calibri', 16), bd=16)
+txt_lastname.grid(row=1, stick="e")
+txt_gender = Label(Forms, text="Gender:", font=('Calibri', 16), bd=16)
+txt_gender.grid(row=2, stick="e")
+txt_address = Label(Forms, text="Address:", font=('Calibri', 16), bd=16)
+txt_address.grid(row=3, stick="e")
+txt_username = Label(Forms, text="Username:", font=('Calibri', 16), bd=16)
+txt_username.grid(row=4, stick="e")
+txt_password = Label(Forms, text="Password:", font=('Calibri', 16), bd=16)
+txt_password.grid(row=5, stick="e")
 txt_result = Label(Buttons)
 txt_result.pack(side=TOP)
 
@@ -192,16 +193,14 @@ btn_delete = Button(Buttons, width=10, text="Delete", command=Delete)
 btn_delete.pack(side=LEFT)
 btn_exit = Button(Buttons, width=10, text="Exit", command=Exit)
 btn_exit.pack(side=LEFT)
-
 #==================================LIST WIDGET========================================
 scrollbary = Scrollbar(Right, orient=VERTICAL)
 scrollbarx = Scrollbar(Right, orient=HORIZONTAL)
-tree = ttk.Treeview(Right, columns=("MemberID", "Firstname", "Lastname", "Gender", "Address", "Username", "Password"), selectmode="extended", height=500, yscrollcommand=scrollbary.set, xscrollcommand=scrollbarx.set)
+tree = ttk.Treeview(Right, columns=("Firstname", "Lastname", "Gender", "Address", "Username", "Password"), selectmode="extended", height=500, yscrollcommand=scrollbary.set, xscrollcommand=scrollbarx.set)
 scrollbary.config(command=tree.yview)
 scrollbary.pack(side=RIGHT, fill=Y)
 scrollbarx.config(command=tree.xview)
 scrollbarx.pack(side=BOTTOM, fill=X)
-tree.heading('MemberID', text="MemberID", anchor=W)
 tree.heading('Firstname', text="Firstname", anchor=W)
 tree.heading('Lastname', text="Lastname", anchor=W)
 tree.heading('Gender', text="Gender", anchor=W)
@@ -209,15 +208,13 @@ tree.heading('Address', text="Address", anchor=W)
 tree.heading('Username', text="Username", anchor=W)
 tree.heading('Password', text="Password", anchor=W)
 tree.column('#0', stretch=NO, minwidth=0, width=0)
-tree.column('#1', stretch=NO, minwidth=0, width=0)
-tree.column('#2', stretch=NO, minwidth=0, width=80)
-tree.column('#3', stretch=NO, minwidth=0, width=120)
-tree.column('#4', stretch=NO, minwidth=0, width=80)
-tree.column('#5', stretch=NO, minwidth=0, width=150)
+tree.column('#1', stretch=NO, minwidth=0, width=80)
+tree.column('#2', stretch=NO, minwidth=0, width=120)
+tree.column('#3', stretch=NO, minwidth=0, width=80)
+tree.column('#4', stretch=NO, minwidth=0, width=150)
+tree.column('#5', stretch=NO, minwidth=0, width=120)
 tree.column('#6', stretch=NO, minwidth=0, width=120)
-tree.column('#7', stretch=NO, minwidth=0, width=120)
 tree.pack()
-tree.bind('<Double-Button-1>', OnSelected)
 
 #==================================INITIALIZATION=====================================
 if __name__ == '__main__':
