e72c675c07dbfbed5949a94650613e26f5bf54fe
Sangeet Shrestha <82662886+sangeeetttt@users.noreply.github.com>
2021-10-04 14:43:55 +0545

    TESTING done

---
+++ test_AMS.py
@@ -0,0 +1,31 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+    def test_create(self):
+        self.fail()
+
+
+class Test(TestCase):
+    def test_update(self):
+        self.fail()
+
+
+class Test(TestCase):
+    def test_delete(self):
+        self.fail()
+
+
+class Test(TestCase):
+    def test_exit(self):
+        self.fail()
+
+
+class Test(TestCase):
+    def test_read(self):
+        self.fail()
+
+
+class Test(TestCase):
+    def test_database(self):
+        self.fail()
